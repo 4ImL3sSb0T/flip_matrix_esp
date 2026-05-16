3bcfb037a375ad91f3b163fdf10eba4c56fb2816
4ImL3sSb0T <wansheng06@outlook.com>
2026-05-16 16:06:51 +0800

    fix: update UART configuration to use UART_NUM_2 and correct baud rate

--- src/bsp/uart/uart_async.c
+++ src/bsp/uart/uart_async.c
@@ -2,6 +2,7 @@
 #include "driver/gpio.h"
 #include "uart_async.h"
 #include "service/tools/common_def.h"
+#include "esp_log.h"
 
 #define UART_RX_BUF_SIZE 2048
 #define UART_TX_BUF_SIZE 2048
@@ -13,22 +14,22 @@ const char *TAG = "uart_async";
 exit_code_t uart_async_init(void)
 {
     uart_config_t cfg = {
-        .baud_rate = 921600,
+        .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
         .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
         .source_clk = UART_SCLK_DEFAULT
     };
-    ESP_ERROR_CHECK(uart_param_config(UART_NUM_1, &cfg));
-    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, UART_RX_BUF_SIZE, UART_TX_BUF_SIZE, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_2, &cfg));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, UART_RX_BUF_SIZE, UART_TX_BUF_SIZE, 16, NULL, 0));
     return EXIT_OK;
 }
 
 exit_code_t uart_async_deinit(void)
 {
-    ESP_ERROR_CHECK(uart_driver_delete(UART_NUM_1));
+    ESP_ERROR_CHECK(uart_driver_delete(UART_NUM_2));
     return EXIT_OK;
 }
 
@@ -38,12 +39,17 @@ exit_code_t uart_async_start(void) {
 
 exit_code_t uart_async_write(const uint8_t* data, const uint32_t len, const TickType_t timeout) {
     if (len == 0) return EXIT_OK;
-    const int sent = uart_write_bytes(UART_NUM_1, data, len);
+    const int sent = uart_write_bytes(UART_NUM_2, data, len);
     if (sent != len)
         return EXIT_FAIL;
     return EXIT_OK;
 }
 
 size_t uart_async_read(uint8_t* data, const uint32_t len, const TickType_t timeout) {
-    return uart_read_bytes(UART_NUM_1, data, len, timeout);
+    size_t read_len = uart_read_bytes(UART_NUM_2, data, len, timeout);
+    if (read_len != 0) {
+        ESP_LOGI(TAG, "Read %d bytes:", read_len);
+        ESP_LOG_BUFFER_HEXDUMP(TAG, data, read_len, ESP_LOG_INFO);
+    }
+    return read_len;
 }
