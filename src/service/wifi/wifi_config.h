83567670e053ea6371101633440c11c27bbeabd6
4ImL3sSb0T <wansheng06@outlook.com>
2026-05-23 14:21:15 +0800

    feat: 更新 WiFi 凭据，添加 SSID 和密码

--- src/service/wifi/wifi_config.h
+++ src/service/wifi/wifi_config.h
@@ -2,7 +2,7 @@
 #define _WIFI_CONFIG_H_
 
 // WiFi 凭据 — 请填入你的 SSID 和密码
-#define WIFI_SSID      ""
-#define WIFI_PASSWORD   ""
+#define WIFI_SSID      "TX5PRO"
+#define WIFI_PASSWORD   "0d000721"
 
 #endif
